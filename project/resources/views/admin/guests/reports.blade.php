c34e967e89c60b27746f502ce431e48a3e9875bb
ztarikul <ztarikulislam@gmail.com>
2021-06-21 18:16:14 +0600

    image solve

--- project/resources/views/admin/guests/reports.blade.php
+++ project/resources/views/admin/guests/reports.blade.php
@@ -28,9 +28,8 @@
       <td>{{$guest->user_name}}</td>
       <td>{{$guest->phone_number}}</td>
         <!-- Button trigger modal -->
-      <td><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-      <img width="50" height="50" src="/images/{{$guest->guest_image}}">
-      </button></td>
+     
+
  
       <td><img  class="zoom" width="50" height="50" src="/storage/{{$guest->guest_image}}"></td>
 
